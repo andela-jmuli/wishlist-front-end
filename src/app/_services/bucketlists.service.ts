83b91dfbea209c81e2f602838072a86bc68f2467
andela-jmuli <joseph.muli@andela.com>
2016-12-07 22:04:35 +0300

    [Feature] Add bucketlist service logic

---
+++ src/app/_services/bucketlists.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { Bucketlist } from '../models/bucketlist';
+
+@Injectable()
+export class UsersService {
+
+  constructor(private http: Http) { }
+  
+
+  create_bucketlist(bucketlist: Bucketlist){
+    return this.http.post('https://wishlistcp.herokuapp.com/bucketlists/', bucketlist, this.jwt()).map((response: Response) => response.json());
+  }
+  get_all_bucketlists(){
+    return this.http.get('https://wishlistcp.herokuapp.com/bucketlists/', this.jwt()).map((response: Response) => response.json());
+  }
+  get_single_bucketlist(id: number){
+    return this.http.get('https://wishlistcp.herokuapp.com/bucketlists/'+ id, this.jwt()).map((response: Response) => response.json());
+  }
+  update_bucketlist(bucketlist: Bucketlist){
+    return this.http.put('https://wishlistcp.herokuapp.com/bucketlists/'+ bucketlist.id, bucketlist, this.jwt()).map((response: Response) => response.json());
+  }
+  delete_bucketlist(id: number){
+    return this.http.delete('https://wishlistcp.herokuapp.com/bucketlists/'+ id, this.jwt()).map((response: Response) => response.json());
+  }
+
+  // helper method for auth
+  private jwt(){
+    let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    if (currentUser && currentUser.auth_token){
+      let headers = new Headers({ 'Authorization': 'Token '+ currentUser.auth_token });
+      headers.append('Access-Control-Allow-Origin', '*');
+      return new RequestOptions({ headers: headers});
+    }
+  }
+}
---
+++ src/app/models/bucketlist.ts
@@ -0,0 +1,8 @@
+export class Bucketlist {
+    id: number;
+    name: string;
+    description: string;
+    date_created: string;
+    date_modified: string;
+
+}
