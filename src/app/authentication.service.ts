8ab0b8a39210ff1297a3a9965b2d266495c6d8e4
andela-jmuli <joseph.muli@andela.com>
2016-12-07 14:17:04 +0300

    [Feature] Add authentication(login) and registration

---
+++ src/app/authentication.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import { Router, NavigationStart } from '@angular/router';
+import 'rxjs/Rx';
+
+@Injectable()
+export class AuthenticationService {
+
+  constructor(private http: Http) { }
+
+  login(username: string, password: string){
+    return this.http.post('https://wishlistcp.herokuapp.com/auth/login/', JSON.stringify({username: username, password: password})).map(
+      (response: Response) => {
+        let user = response.json();
+        if (user && user.auth_token){
+          localStorage.setItem('currentUser', JSON.stringify(user));
+        }
+      }
+    );
+  }
+
+  logout(){
+    localStorage.removeItem('currentUser')
+  }
+
+}
---
+++ src/app/users.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { User } from './models/user';
+
+@Injectable()
+export class UsersService {
+
+  constructor(private http: Http) { }
+
+  create(user: User){
+    return this.http.post('https://wishlistcp.herokuapp.com/auth/register/', user)
+  }
+}
